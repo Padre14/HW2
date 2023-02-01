78f70955fca042bde748062443cfe0b00ef8684e
Pavel <athletic.1994@mail.ru>
2023-02-01 20:45:56 +0300

    final2  HW

--- Program.cs
+++ Program.cs
@@ -21,13 +21,13 @@ if (int.TryParse(strNumber, out number))
     }
 }
 
-// Задача 13. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// Задача 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
 // 6 -> да
 // 7 -> да
 // 1 -> нет
     
-Console.WriteLine("Task 13");
+Console.WriteLine("Task 15");
 Console.WriteLine("Введите номер дня недели:");
 string? strNumberDayOfWeek = Console.ReadLine();
 int numberDayOfWeek = 0;
@@ -48,12 +48,12 @@ if (int.TryParse(strNumberDayOfWeek, out numberDayOfWeek))
             Console.WriteLine("Введиде число от 1 до 7");
         }
 }
-// Задача 10. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.WriteLine("Task 10");
+Console.WriteLine("Task 13");
 Console.WriteLine("Enter numbers :");
 string? strNumberA = Console.ReadLine();
 int numberA = 0;
